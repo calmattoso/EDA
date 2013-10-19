6473eee0f12ca821a69266cec926406aee115a94
Carlos Mattoso <calmattoso@hotmail.com>
2013-10-19 15:26:10 -0300

    Created interface for Hash Table.

---
+++ hash/hash.h
@@ -0,0 +1,21 @@
+#ifndef HASH_H
+#define HASH_H
+
+typedef struct hash hash;
+
+typedef enum {
+
+  hash_Ok ,
+  hash_NoMem ,
+  hash_Found ,
+  hash_NotFound ,
+  hash_ErrParm
+
+} hash_ret;
+
+hash_ret hash_create(hash ** h, unsigned int capacity);
+hash_ret hash_insert(hash * h,  char * str);
+hash_ret hash_search(hash * h,  char * str);
+hash_ret hash_remove(hash * h,  char * str);
+
+#endif
